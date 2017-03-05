706f4343fcd03f0c18f9cd706713ba5bfdbc72ab
epribs <ericpribramsky@gmail.com>
2017-03-04 22:27:38 -0600

    Adding files

--- assets/js/app2.js
+++ assets/js/app2.js
@@ -1,13 +1,20 @@
 var topics = ["Archer", "Family Guy", "Futurama", "Rick and Morty", "Metalocalypse"];
+render();
+
+function render(){
+$("#buttons").empty();
 for (var i = 0; i < topics.length; i++) {
 	var button = $("<button>")
-	button.addClass("btn btn-success gifBtn");
+	button.addClass("btn btn-success");
 	button.attr("data-value", topics[i]);
+	button.attr("id", "gifBtn");
 	button.text(topics[i]);
+
 	$("#buttons").append(button);
 }
+}
 
-$(".gifBtn").on("click", function() {
+function gifs() {
 	$("#gifs").empty();
 	var btnVal = $(this).attr("data-value").split(" ").join("+");
 	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + btnVal + "&rating=pg-13&limit=12&api_key=dc6zaTOxFJmzC";
@@ -21,32 +28,27 @@ $(".gifBtn").on("click", function() {
 		var results = response.data;
 		console.log(results);
 		for (var i = 0; i < results.length; i++){
-			var imgDiv = $("<button>");
-			var gifStill = $("<img>");
-			gifStill.attr("src", results[i].images.fixed_height_small_still.url);
-			imgDiv.append(gifStill);
-			imgDiv.attr("data-toggle", "modal");
-			imgDiv.attr("data-target", "#myModal");
-			imgDiv.attr("id", results.i);
-			console.log(results[i]);
-			$("#gifs").append(imgDiv);
-
-			//For Modal
-			var gifModalBody = $(".modal-body");
-			gifModalBody.append(gifDiv);
 			var gifDiv = $("<div>");
-			var resultsData = $("imgDiv").attr("id");
-			console.log("RESULTDATA" + resultsData);
-			// var rating = $("<p>").text("Rating: " + resultsData.rating);
+			var rating = $("<p>").text("Rating: " + results[i].rating);
 			var gifImg = $("<img>");
-			gifDiv.addClass("col-sm-4");
-			// gifImg.attr("src", resultsData.images.fixed_height.url);
-			gifImg.addClass("imgCenter");
-			gifDiv.append(gifImg);
-			// gifDiv.append(rating);
-		}
+			gifImg.attr("src", results[i].images.fixed_height.url);
 
-		
-			
+			gifDiv.addClass("col-sm-4");
+			gifDiv.append(gifImg);
+			gifDiv.append(rating);
+
+			$("#gifs").append(gifDiv);
+		}
   })
+}
+
+$("#add-show").on("click", function(event) {
+	event.preventDefault();
+	var show = $("#show-input").val().trim();
+	$("#show-input").append().val("");
+	topics.push(show);
+	render();
 });
+
+$(document).on("click", "#gifBtn", gifs);
+render();
